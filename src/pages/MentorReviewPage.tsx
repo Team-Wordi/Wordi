522651bdbf7f42873feb35dc3b6ed992e5e79886
JuYoung <ken44929@gmail.com>
2021-11-03 10:38:26 +0900

    Feat: 초기 라우터 설정 [#1]

---
+++ src/pages/MentorReviewPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MentorReviewPage = () => {
+  return <div>MentorReviewPage</div>;
+};
+
+export default MentorReviewPage;
---
+++ src/utils/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  ROOT: '/',
+  NATION: {
+    MAIN: '/:nation',
+    NAME: '/:nation/:name',
+    APPLICATION: '/:nation/:name/application',
+  },
+  // temporary route
+  REVEIWS: '/reviews',
+};
