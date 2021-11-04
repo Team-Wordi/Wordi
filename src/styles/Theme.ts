feba15dc91e20cbd24d5eba6a1bf1d571aa19f5c
JuYoung <ken44929@gmail.com>
2021-11-04 15:11:31 +0900

    Style: Theme.ts에 gray 관련 색상들 추가

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -2,4 +2,11 @@ export const COLORS = {
   primary: '#575FCC',
   white: '#FFFFFF',
   black: '#283244',
+  yellow: '#FABF48',
+  green: '#8FD8B5',
+  gray_01: '#E8E8E9',
+  gray_02: '#C4C4C4',
+  gray_03: '#929292',
+  gray_04: '#666666',
+  gray_05: '#323232',
 };
