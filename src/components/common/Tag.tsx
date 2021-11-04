8de6a9bebe5ebe2ed8c807aa0a523a61c1e151c9
JuYoung <ken44929@gmail.com>
2021-11-04 14:24:37 +0900

    Feat: [#2] Tag component 추가

--- src/components/common/Tag.tsx
+++ src/components/common/Tag.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import { COLORS } from 'styles/Theme';
 
-const Tag = () => {
-  return <div>Tag</div>;
+const Container = styled.div`
+  font-family: 'Pretendard';
+  font-weight: 400;
+  display: inline-block;
+  padding: 6px 16px;
+  border: 2px solid ${COLORS.primary};
+  color: ${COLORS.primary};
+  border-radius: 6px;
+`;
+
+interface TagProps {
+  text?: string | null;
+}
+
+const Tag = ({ text }: TagProps) => {
+  return <>{text && <Container>{text}</Container>}</>;
 };
 
 export default Tag;
