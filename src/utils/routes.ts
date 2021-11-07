42e1c08f72765b5c6353adcdc57523925c22b771
JuYoung <ken44929@gmail.com>
2021-11-07 16:57:02 +0900

    Fix: 사용하지 않는 routes 제거

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,10 +1,6 @@
 export const ROUTES = {
   ROOT: '/',
-  NATION: {
-    MAIN: '/:nation',
-    NAME: '/:nation/:name',
-    APPLICATION: '/:nation/:name/application',
-  },
+
   // temporary route
   REVEIWS: '/reviews',
   APPLICATION: '/application',
