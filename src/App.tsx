b57e06e24c67a2e8ca76d0dfe78175c8ebd7560a
JuYoung <ken44929@gmail.com>
2021-11-10 14:13:36 +0900

    Fix: GlobalStyle Container에서 MainPage를 제외

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ const App: React.FC<any> = () => {
   return (
     <Switch>
       <>
+        <Route exact strict path={ROUTES.ROOT} component={MainPage} />
         <Container>
-          <Route exact strict path={ROUTES.ROOT} component={MainPage} />
           <Route exact path={ROUTES.REVIEWS} component={MentorReviewPage} />
           <Route exact path={ROUTES.APPLICATION} component={ApplicationPage} />
           <Route exact path={ROUTES.MENTOR_COLUMN} component={MentorColumnPage} />
