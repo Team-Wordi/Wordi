55a4b20d577f2238ac5b0bb9d31a03b8c1b0ffc7
Jsim6342 <snowdrop6342@gmail.com>
2021-11-23 15:07:23 +0900

    Feat: [#92] 메인 페이지 초기 API 구현

---
+++ server/wordi/src/main/java/com/pm/wordi/controller/dto/CommonDto.java
@@ -0,0 +1,63 @@
+package com.pm.wordi.controller.dto;
+
+import com.pm.wordi.domain.mentor.Mentor;
+import com.pm.wordi.domain.review.Review;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+public class CommonDto {
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class MainRes{
+        private List<ReviewDTO> reviewDTOList;
+        private List<MentorDTO> mentorDTOList;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ReviewDTO{
+        private String mentorNation;
+        private String mentorNickname;
+        private Long mentorPeriod;
+        private String content;
+        private String userNickname;
+        private LocalDateTime writeDate;
+
+        public ReviewDTO(Review review) {
+            this.mentorNation = review.getMentor().getNation();
+            this.mentorNickname = review.getMentor().getUser().getNickname();
+            this.mentorPeriod = ChronoUnit.MONTHS.between(review.getMentor().getStartDate(), review.getMentor().getEndDate());
+            this.content = review.getContent();
+            this.userNickname = review.getUser().getNickname();
+            this.writeDate = review.getUpdated();
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class MentorDTO{
+        private String profileImageUrl;
+        private String nation;
+        private String nickname;
+        private Long period;
+
+        public MentorDTO(Mentor mentor) {
+            this.profileImageUrl = mentor.getProfileImageUrl();
+            this.nation = mentor.getNation();
+            this.nickname = mentor.getUser().getNickname();
+            this.period = ChronoUnit.MONTHS.between(mentor.getStartDate(), mentor.getEndDate());
+        }
+    }
+
+
+}
---
+++ server/wordi/src/main/java/com/pm/wordi/service/CommonService.java
@@ -0,0 +1,43 @@
+package com.pm.wordi.service;
+
+import com.pm.wordi.controller.dto.CommonDto;
+import com.pm.wordi.domain.BaseStatus;
+import com.pm.wordi.domain.mentor.MentorRepository;
+import com.pm.wordi.domain.mentoring.MentoringRepository;
+import com.pm.wordi.domain.review.ReviewRepository;
+import com.pm.wordi.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.pm.wordi.controller.dto.CommonDto.*;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CommonService {
+
+    private final UserRepository userRepository;
+    private final MentorRepository mentorRepository;
+    private final MentoringRepository mentoringRepository;
+    private final ReviewRepository reviewRepository;
+
+    @Transactional(readOnly = true)
+    public MainRes getMain(Long userId) {
+        List<ReviewDTO> reviewDTOList = reviewRepository.findTop20ByStatusOrderByIdDesc(BaseStatus.ACTIVE).stream()
+                .map(ReviewDTO::new)
+                .collect(Collectors.toList());
+
+        // TODO. 멘토링 내역에서 일주일 전 부터 현재까지 멘토 기준으로 멘토링 개수를 실셈하여 내림차순 정렬 + 해당 mentorId를 List로 반환
+
+        List<MentorDTO> mentorDTOList = mentorRepository.searchProfileList().stream()
+                .map(MentorDTO::new)
+                .collect(Collectors.toList());
+
+        return new MainRes(reviewDTOList, mentorDTOList);
+
+    }
+}
