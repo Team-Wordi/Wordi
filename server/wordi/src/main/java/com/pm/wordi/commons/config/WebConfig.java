faebca173a24e008dbcc2dffd300c0acde36765a
Jsim6342 <snowdrop6342@gmail.com>
2021-11-26 11:54:04 +0900

    Fix: [#145] CORS 설정 및 Preflight Request 로직 추가

--- server/wordi/src/main/java/com/pm/wordi/commons/config/WebConfig.java
+++ server/wordi/src/main/java/com/pm/wordi/commons/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.pm.wordi.commons.config;
 import com.pm.wordi.commons.interceptor.LoginCheckInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginCheckInterceptor)
                 .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
     }
 }
