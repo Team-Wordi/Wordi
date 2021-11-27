6f3c1b92148041913300f019f621b2465e08c5b5
JuYoung <ken44929@gmail.com>
2021-11-27 21:52:59 +0900

    Style: [#157] MentorDetailPage 더보기 > 추가

--- client/src/components/MentorDetail/Reviews.tsx
+++ client/src/components/MentorDetail/Reviews.tsx
@@ -5,12 +5,20 @@ import ReviewIcon from 'components/icon/ReviewIcon';
 import { reviewData } from 'constants/dummy';
 import ReviewCard from 'components/common/ReviewCard';
 import FlatList from 'components/common/FlatList';
+import RightIcon from 'components/icon/RightIcon';
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 16px;
+
+  margin-top: 40px;
+  margin-bottom: 18px;
+`;
 
 const TitleWrapper = styled.div`
   display: flex;
   align-items: center;
-  padding: 0 16px;
-  margin-bottom: 18px;
 
   & > svg {
     margin-right: 6px;
@@ -24,6 +32,19 @@ const Title = styled.h2`
   line-height: 19px;
 `;
 
+const ShowMore = styled.div`
+  display: flex;
+  align-items: center;
+
+  color: ${COLORS.gray_03};
+  font-size: 11px;
+  line-height: 13px;
+
+  & > svg {
+    margin-left: 2px;
+  }
+`;
+
 const Reviews = () => {
   const [reviews, setReviews] = useState<any>([]);
 
@@ -31,12 +52,20 @@ const Reviews = () => {
     setReviews(reviewData);
   }, []);
 
+  const goColumnDetailPage = () => {};
+
   return (
     <>
-      <TitleWrapper>
-        <ReviewIcon size={18} color={COLORS.primary} />
-        <Title>워디 후기</Title>
-      </TitleWrapper>
+      <TitleContainer>
+        <TitleWrapper>
+          <ReviewIcon size={18} color={COLORS.primary} />
+          <Title>리얼 멘토링 후기!</Title>
+        </TitleWrapper>
+        <ShowMore>
+          더 보기
+          <RightIcon size={14} color={COLORS.gray_03} onClick={goColumnDetailPage} />
+        </ShowMore>
+      </TitleContainer>
       <FlatList>
         {reviews.map((data: any) => (
           <ReviewCard review={data.review} reviewer={data.reviewer} date={data.date} />
