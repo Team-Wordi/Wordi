0c95a0f4b7e997f3321573c80339a20f7193caf3
Jsim6342 <snowdrop6342@gmail.com>
2021-12-13 11:45:34 +0900

    Fix: [#183] gradle log4j 버전 업그레이드 설정 추가

--- server/wordi/src/main/java/com/pm/wordi/controller/MentorApiController.java
+++ server/wordi/src/main/java/com/pm/wordi/controller/MentorApiController.java
@@ -3,6 +3,7 @@ package com.pm.wordi.controller;
 
 import com.pm.wordi.service.MentorService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,6 +18,7 @@ import java.util.List;
 import static com.pm.wordi.commons.utils.constants.ResponseConstants.*;
 import static com.pm.wordi.controller.dto.MentorDto.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/app/mentors")
 @RequiredArgsConstructor
